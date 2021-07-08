4c54ed2461c81e8535e93b4c26aa1a5d15a4a7bc
Ruukasu <63557605+RuukasuGIT@users.noreply.github.com>
2021-07-08 21:28:26 +0100

    Added Master File

---
+++ file.py
@@ -0,0 +1,14 @@
+import time
+
+string = input("String: ")
+delay = float(input("Delay: "))
+
+def typewriter(x, z):
+    y = ""
+    for i in x:
+        y += i
+        print("\r", y, end="")
+        time.sleep(z)
+
+
+typewriter(string, delay)
